9da20fcb0459fb53d38d9dc0f183237480173897
Emile Van Gysel (emile.vangysel@student.uclouvain.be) <vangysel@dragon2.umons.ac.be>
2026-03-12 10:29:50 +0100

    conda env creation w. snakemake

--- prebuild_envs.sh
+++ prebuild_envs.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
-# Usage: ./prebuild_envs.sh 
-# Builds only specified YAMLs into .snakemake/conda/<hash>_
+SNAKEMAKE=/home/ucl/inma/vangysel/.conda/envs/greta/bin/snakemake
 
-set -euo pipefail
-
-SNK_CONDA=".snakemake/conda"
-mkdir -p "$SNK_CONDA"
-
-# list of config files that require a conda env
-CONDA_ENV=("workflow/envs/scdori.yaml" "workflow/envs/dictys.yaml" "workflow/envs/scgpt.yaml")
-
-if [ "$#" -eq 0 ]; then
-    echo "Error: Please provide at least one conda YAML file to prebuild."
-    exit 1
-fi
-
-for yaml in "$@"; do
-    if [ ! -f "$yaml" ]; then
-        echo "Warning: YAML file '$yaml' not found, skipping."
-        continue
-    fi
-
-    # Compute hash (same as Snakemake does)
-    HASH=$(md5sum "$yaml" | awk '{print $1}')_
-    TARGET="$SNK_CONDA/$HASH"
-
-    echo "Prebuilding Conda environment for $yaml -> $TARGET"
-
-    # Remove any leftover folder / yaml
-    rm -rf "$TARGET" "$SNK_CONDA/$HASH.yaml"
-
-    # Create environment
-    mamba env create --file "$yaml" --prefix "$TARGET"
-
-    echo "Done: $TARGET"
-done
-
-echo "All requested environments prebuilt in $SNK_CONDA"
+$SNAKEMAKE --ri --conda-create-envs-only \
+    --conda-frontend conda \
+    --profile config/slurm/ \
+    dts/hg38/pbmc10k/cases/all/runs/o_dictys.o_dictys.o_dictys.o_dictys.mdl.csv \
+    dts/hg38/pbmc10k/cases/all/runs/o_scdori.o_scdori.o_scdori.o_scdori.mdl.csv \
+    dts/hg38/pbmc10k/cases/all/runs/o_scgpt.o_scgpt.o_scgpt.o_scgpt.mdl.csv
